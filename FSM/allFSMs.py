0df138c20c0d8e73242756d46718210578425aa7
Dake69 <brawlplayer914@gmail.com>
2025-05-06 22:29:02 +0300

    add photo

--- FSM/allFSMs.py
+++ FSM/allFSMs.py
@@ -4,3 +4,7 @@ class Registration(StatesGroup):
     full_name = State()
     phone_number = State()
     previous_school = State()
+
+
+class PhotoStates(StatesGroup):
+    waiting_for_photo = State()
---
+++ keyboards/photo.py
@@ -0,0 +1,8 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+def get_main_menu_keyboard():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="🏠 Повернутися до головного меню", callback_data="main_menu")
+        ]
+    ])
